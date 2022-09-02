bee8676a8297fe96efc13210e71afab4da6f2b50
Ahmed Mohamed <ahmedysatti@gmail.com>
2022-09-02 08:42:18 +0100

    Tutorial file

--- Tuples.py
+++ Tuples.py
@@ -1,2 +1,25 @@
-coordinates = (4, 5)
-print(coordinates[1])
+random_items = (4, 5, "Cake", "Grapes", 34)
+print (random_items)
+
+#Checking if item is in tuple
+#if "Peanut" in random_items:
+    #print("Item in list")
+#else:
+    #print("Item not in list")
+    
+#Prints number of items in tuple
+#print(len(random_items))
+
+#Counts how many times an element exists in tuple
+#print(random_items.count('Peanut'))
+
+#Prints the index of an element
+#print(random_items.index(34))
+
+#Converts a tuple into a list
+#my_list = list(random_items)
+#print(my_list)
+
+#Converts a list into a tuple
+#my_tuple = tuple(random_items)
+#print(my_tuple)
