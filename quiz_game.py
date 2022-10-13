c64ae456536a71cadf7a7fe8fb3692d40c0a1fd7
Ahmed Mohamed <ahmedysatti@gmail.com>
2022-10-13 11:54:58 +0100

    Updated game file

--- quiz_game.py
+++ quiz_game.py
@@ -1,48 +1,60 @@
-print("Welcome to guess the right answer!")
-score = 0
+print("Welcome to my quiz game!")
+right_question = 0
 
-# A list containing the right answer
-correct_answer = ["1 trillion dollars", "1998", "1558", "2012", "Eiffel & Cie"]
+def count_score(num_questions, score = 25):
+    if right_question != num_questions:
+        print("Wrong, the questions you got right is: " + str(right_question))
+    if right_question >= 3:
+        print("Congratulations you passed!")
+    if right_question <= 2:
+        print("Sorry, you failed the test.")
+    print(num_questions * score)
+    return (right_question)
+
+
+# A list containing the right answers
+correct_answer = ["$1 trillion", "1998", "1558", "2012", "Eiffel & Cie"]
+    
     
 question_one = input("What much is Apple worth? ")
 if question_one == correct_answer[0]:
     print("Correct!")
-    score = score + 25
+    right_question = right_question + 1
 else:
     print("Sorry, the right answer is " + correct_answer[0])
-    score = score - 25
+    #End
     
 question_two = input("What year did Michael Jordan win his last championship? ")
 if question_two == correct_answer[1]:
     print("Correct!")
-    score = score + 25
+    right_question = right_question + 1
 else:
     print("Sorry, the right answer is " + correct_answer[1])
-    score = score - 25
+    #End
     
 question_three = input("What year did Queen Elizabeth I become queen? ")
 if question_three == correct_answer[2]:
     print("Correct!")
-    score = score + 25
+    right_question = right_question + 1
 else:
     print("Sorry, the right answer is " + correct_answer[2])
-    score = score - 25
+    #End
     
 question_four = input("What year did Frank Ocean dropped his album channel ORANGE? ")
 if question_four == correct_answer[3]:
     print("Correct!")
-    score = score + 25
+    right_question = right_question + 1
 else:
     print("Sorry, the right answer is " + correct_answer[3])
-    score = score - 25
+    #End
     
-question_five = input("Who designed the Eiffel Tower? ")
+question_five = input("Who designed & built the Eiffel Tower? ")
 if question_five == correct_answer[4]:
     print("Correct!")
-    score = score + 25
+    right_question = right_question + 1
 else:
     print("Sorry, the right answer is " + correct_answer[4])
-    score = score - 25
+    #End
     
-print("Your score is " + str(score) + " points.")
+count_score(num_questions = int(input("Enter the amount of questions you got right: ")))
 print("Thank you for playing :)")
